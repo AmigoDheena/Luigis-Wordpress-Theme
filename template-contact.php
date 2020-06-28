93ea2843d6ccdcc8bfdfda3fe85179d1c5c63898
AmigoDheena <amigodheena@gmail.com>
2020-06-28 19:53:46 +0530

    wpcf7

--- template-contact.php
+++ template-contact.php
@@ -30,18 +30,18 @@ $say_hello = get_field('say_hello');
             <h5 class="mt-10 mb-30"><?php echo $say_hello['long_title']; ?></h5>
             <p class="mx-w-700x mlr-auto"><?php echo $say_hello['description']; ?></p>
         </div>
-
         <form class="form-style-1 placeholder-1">
-            <div class="row">
+            <!--<div class="row">
                 <div class="col-md-6"> <input class="mb-20" type="text" placeholder="Name">  </div>
                 <div class="col-md-6"> <input class="mb-20" type="text" placeholder="E-mail">  </div>
                 <div class="col-md-12"><input class="mb-20" type="text" placeholder="Subject">  </div>
                 <div class="col-md-12">
                     <textarea class="h-200x ptb-20" placeholder="Message"></textarea>
                 </div>
-            </div><!-- row -->
-            <h6 class="center-text mtb-30"><a href="#" class="btn-primaryc plr-25"><b>SEND MESSAGE</b></a></h6>
-        </form>
+            </div>
+            <h6 class="center-text mtb-30"><a href="#" class="btn-primaryc plr-25"><b>SEND MESSAGE</b></a></h6> -->
+            <?php echo do_shortcode('[contact-form-7 id="139" title="Contact form 1"]'); ?>
+        </form> 
     </div><!-- container -->
 </section>
 
